b7cacfb55b5f7816f3c4554e5069452f89234972
Raphael <raphael.meudec@gmail.com>
2016-11-21 14:58:12 +0100

    Modifications for stupidsolver

---
+++ build/v0-12-406.sol
@@ -0,0 +1,25 @@
+# générée le : TODO 
+
+nom velib_0
+desequilibre 12
+distance 406
+
+# Circuit associé à la remorque r1 de capa 5
+#       id, charge_init, desequ, longueur
+circuit r1        0        2       164
+  s1 0
+  s3 -3
+  s5 -2
+  s7 0
+end
+
+# Circuit associé à la remorque r2 de capa 4
+#       id, charge_init, desequ, longueur
+circuit r2        4        10       242
+  s2 4
+  s4 -1
+  s6 -3
+  s8 4
+end
+
+
