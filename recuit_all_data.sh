1e0dc60a70459b86cee1b6060745d4f876122889
Raphael <raphael.meudec@gmail.com>
2017-01-03 20:36:19 +0100

    Adding level option as  for recuit_all_data script

--- recuit_all_data.sh
+++ recuit_all_data.sh
@@ -3,23 +3,23 @@ echo "Temperature initiale : " . $1
 echo "Nombre d'itérations pour chaque température : " . $2
 echo "Start running all data files"
 echo "File 0"
-./run --descent ../data/v0.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v0.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 1"
-./run --descent ../data/v1.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v1.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 2"
-./run --descent ../data/v2.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v2.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 3"
-./run --descent ../data/v3.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v3.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 4"
-./run --descent ../data/v4.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v4.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 5"
-./run --descent ../data/v5.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v5.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 6"
-./run --descent ../data/v6.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v6.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 7"
-./run --descent ../data/v7.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v7.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 8"
-./run --descent ../data/v8.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v8.dat --temperature $1 --explore recuit --itermax $2 --level $3
 echo "File 9"
-./run --descent ../data/v9.dat --temperature $1 --explore recuit --itermax $2 --level 0
+./run --descent ../data/v9.dat --temperature $1 --explore recuit --itermax $2 --level $3
 cd ..
